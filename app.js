3db9e2334ec3be4d144d3a16e739ff2967b14c18
Ahmad <labibahmad136@gmail.com>
2024-01-15 12:36:33 +0600

    quiz app initial

---
+++ app.js
@@ -0,0 +1,14 @@
+const questions = [
+    {
+        topic: 'science',
+        question: 'What color is the sky?',
+        possibleAnswers: ['blue', 'green', 'red'],
+        correctAnswer: 'blue'
+    },
+    {
+        topic: 'anime',
+        question: 'Which is your favorite anime?',
+        possibleAnswers: ['JJK', 'AOT', 'Deathnote'],
+        correctAnswer: 'JJK'
+    },
+];

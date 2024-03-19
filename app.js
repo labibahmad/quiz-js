e3aea30da2b2a75de9e6d4d3d19d125124b57ee2
Ahmad <labibahmad136@gmail.com>
2024-03-19 14:51:25 +0600

    worked on JS of question spans

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 const questions = [
     {
-        topic: 'science',
-        question: 'What color is the sky?',
-        possibleAnswers: ['blue', 'green', 'red'],
-        correctAnswer: 'blue'
+        topic: "science",
+        question: "What color is the sky?",
+        possibleAnswers: ["blue", "green", "red"],
+        correctAnswer: "blue"
     },
     {
-        topic: 'anime',
-        question: 'Which is your favorite anime?',
-        possibleAnswers: ['JJK', 'AOT', 'Deathnote'],
-        correctAnswer: 'JJK'
+        topic: "anime",
+        question: "Which is your favorite anime?",
+        possibleAnswers: ["JJK", "AOT", "Deathnote"],
+        correctAnswer: "JJK"
     },
 ];
+
+const quizProgress = document.getElementById('quizProgress')
+const questionContainer = document.getElementById('questionContainer')
+const answerContainer = document.getElementById('answerContainer');
+
+let currentQuestionIndex = 0;
+
+function handleQuestion() {
+    //handle quiz progress
+    questions.forEach((question) => {
+        quizProgress.innerHTML += "<span></span>";
+    });
+
+    let spans = document.querySelectorAll('span');
+    for (let i=0; i<= currentQuestionIndex; i++) {
+        spans[i].classList.add('seen');
+    }
+}
+
+handleQuestion();
